9fa88d29a67e2a453ad8760870ebf2f58ad1fe3e
Ravheath <ravitanand2014@gmail.com>
2019-05-14 22:45:41 +0530

    corrected the code

--- Dynamics3.m
+++ Dynamics3.m
@@ -3,9 +3,11 @@ function xdot=Dynamics3(t,x)
 %this function is called by rk4.m
 global I beta; % moment of inertia matrix declared in main code
 q=x(1:4,1); % extracting quaternion 
-w=x(5:7,1); % extracting omega
+delw=x(5:7,1); % extracting omega
 wd=[0;beta;0];
-q_dot=0.5*quat_prod([w;0],q); % calculating the derivative of quaternion
-w_dot=I\(torque(t)-cross(w,I*w)-I*(cross((quat2Rot(q))*w,wd))); % calculating the derivative of omega
-xdot=[q_dot; w_dot]; % concatenating both the derivatives 
+q_dot=0.5*quat_prod([delw;0],q); % calculating the derivative of quaternion
+R=quat2Rot(q);
+w=delw+R*wd;
+delw_dot=I\(torque(t)-cross(w,I*w)-I*(cross((R)*wd,delw))); % calculating the derivative of omega
+xdot=[q_dot; delw_dot]; % concatenating both the derivatives 
 
